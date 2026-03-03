a25d857fa08f55a670d3a9b04c4bb0a813808853
TaLongHo <hotahihi123@gmail.com>
2026-03-03 15:59:34 +0700

    View List Customer for staff, Admin

--- Pet_Center/obj/Release/net8.0/ProductAPI.AssemblyInfo.cs
+++ Pet_Center/obj/Release/net8.0/ProductAPI.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductAPI")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+729597608ae810ec0da8b821d56822b1b328ecc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductAPI")]
