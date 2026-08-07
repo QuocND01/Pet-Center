f082c7c257bd70fa99ff368e29103769df62dec6
tien260604 <nhuttien26062004@gmail.com>
2026-08-07 18:32:40 +0700

    Xoa dto ko can thiet

--- PetCenterClient/Services/Interface/IStatisticsServiceClient.cs
+++
@@ -1,9 +0,0 @@
-﻿using PetCenterClient.DTOs;
-
-namespace PetCenterClient.Services.Interface
-{
-    public interface IStatisticsServiceClient
-    {
-        Task<DashboardStatsDto?> GetDashboardStatsAsync(int? year);
-    }
-}
